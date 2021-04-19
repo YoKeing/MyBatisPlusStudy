9ddf221d83c5f1b5a0164494ae0ea51b96410dee
YoKeing <1171705508@qq.com>
2021-04-19 15:05:53 +0800

    update

--- haha.sh
+++ haha.sh
@@ -3,7 +3,7 @@
 git init
 git add ./*
 git commit -am 'update'
-git remote add origin git@github.com:YoKeing/MyBatisStudy.git
+git remote add origin git@github.com:YoKeing/MyBatisPlusStudy.git
 git push origin master
 
 echo -e '\n\nDown!\n\n'
